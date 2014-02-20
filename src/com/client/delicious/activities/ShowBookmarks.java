2c277d218c597ef7224834f81b93c3f52bfe3116
Adeel Inam <adeel.aqua@gmail.com>
2014-02-20 07:38:53 -0800

    xml response done

--- src/com/client/delicious/activities/ShowBookmarks.java
+++ src/com/client/delicious/activities/ShowBookmarks.java
@@ -1,7 +1,17 @@
 package com.client.delicious.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class ShowBookmarks extends Activity {
+import com.client.delicious.R;
 
+public class ShowBookmarks extends Activity
+{
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		// TODO Auto-generated method stub
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.show_bookmark );
+	}
 }
--- src/com/client/delicious/utilities/AppGlobal.java
+++ src/com/client/delicious/utilities/AppGlobal.java
@@ -1,20 +1,21 @@
 package com.client.delicious.utilities;
 
-public class AppGlobal {
+public class AppGlobal
+{
 
-	public static final String CLIENT_ID = "4ee6eefb6e7b7fdd0c2fc43bbabd6109";
-	public static final String CLIENT_SECRET = "94237dc31e9bc196647fa48368ba6bcf";
-	public static final String APP_SHARED_PREF_ACCESS_TOKEN = "access token";
-	public static final String DELICIOUS_API_OAUTH_URL = "https://avosapi.delicious.com/api/v1/oauth/token?client_id=";
-	public static final String DELICIOUS_API_URL = "api.delicious.com/v1/posts/";
-	public static final String STATUS_SUCESS = "success";
-	public static final String STATUS_FAIL = "invalid_credentials";
-	
-	////Response Status Codes
-	public static final int RESPONSE_SUCCESS = 1;
-	public static final int RESPONSE_FAIL = 2;
-	
-	///DataFetcher Actions Constants
-	public static final String ACTION_DATAFETCHER_SIGNIN = "signIn";
-	public static final String ACTION_DATAFETCHER_GET_LINKS = "fetch";
+	public static final String	CLIENT_ID						= "4ee6eefb6e7b7fdd0c2fc43bbabd6109";
+	public static final String	CLIENT_SECRET					= "94237dc31e9bc196647fa48368ba6bcf";
+	public static final String	APP_SHARED_PREF_ACCESS_TOKEN	= "access token";
+	public static final String	DELICIOUS_API_OAUTH_URL			= "https://avosapi.delicious.com/api/v1/oauth/token?client_id=";
+	public static final String	DELICIOUS_API_URL				= "https://api.delicious.com/v1/posts/";
+	public static final String	STATUS_SUCESS					= "success";
+	public static final String	STATUS_FAIL						= "invalid_credentials";
+
+	// //Response Status Codes
+	public static final int		RESPONSE_SUCCESS				= 1;
+	public static final int		RESPONSE_FAIL					= 2;
+
+	// /DataFetcher Actions Constants
+	public static final String	ACTION_DATAFETCHER_SIGNIN		= "signIn";
+	public static final String	ACTION_DATAFETCHER_GET_LINKS	= "fetch";
 }
